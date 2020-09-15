f0e3b2864687527cbadee22762cdd66b4b16234b
Zach Cullen <zacullen@gmail.com>
2020-09-15 14:14:32 +0000

    Automatically backed up by Learn

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,26 +1,20 @@
 class BST
-  class Node
-    attr_reader :data, :left, :right
+  attr_reader :data, :left, :right
     
-    def initialize(data)
-      @data = data
-      @left = nil
-      @right = nil
-    end
-    
-    def insert(data)
-      if self.data = nil
-        self.data = Node.new(data)
-      elsif data < self.data
-        self.left.insert(data)
-      elsif data > self.data
-        self.right.insert(data)
-      end
-    end
-  end
-  
   def initialize(data)
-    @root = Node.new(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+    
+  def insert(data)
+    if self.data = nil
+      self.data = 
+    elsif data < self.data
+      self.left.insert(data)
+    elsif data > self.data
+      self.right.insert(data)
+    end
   end
   
   def data
