e0956b3d3ea2c57444635e82721a5f319a048464
Zach Cullen <zacullen@gmail.com>
2020-09-15 14:16:45 +0000

    Automatically backed up by Learn

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -3,14 +3,10 @@ class BST
     
   def initialize(data)
     @data = data
-    @left = nil
-    @right = nil
   end
     
   def insert(data)
-    if self.data = nil
-      self.data = 
-    elsif data < self.data
+    if data <= @data
       self.left.insert(data)
     elsif data > self.data
       self.right.insert(data)
